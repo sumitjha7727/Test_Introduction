ab9f872ddff942d2240729457a97f4c6e38481a5
sumitjha7727 <sumitjha7727@gmail.com>
2022-04-16 18:01:32 +0530

    Testing Introduction

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Testing_Introduction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://www.google.com/");
+
+            Thread.Sleep(2000);
+
+            IWebElement SearchElement = driver.FindElement(By.Name("q"));
+
+            SearchElement.SendKeys("Bassetti India");
+
+            IWebElement buttonElement = driver.FindElement(By.Name("btnK"));
+            buttonElement.Click();
+
+            driver.Close();
+        }
+    }
+}
